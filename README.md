710ad9c8c0e6ac387f4a320b1c973a75d1f1e1dd
seungriyu <siso1210@naver.com>
2018-08-28 15:51:18 +0900

    c1

--- README.md
+++
@@ -1,4 +0,0 @@
-# bitcamp-cloud-test
-XXXX
-git 연습용 저장소
-11111111
--- src/bitcamp/Hello.java
+++
@@ -1,9 +0,0 @@
-package bitcamp;
-
-public class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello");
-        
-    }
-    
-}
